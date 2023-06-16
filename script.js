795fbb217b139f356145d9543138d583a1ffab4d
Gabriel Misao Pinheiro Kaminise <117327879+zKaminise@users.noreply.github.com>
2023-06-16 14:19:16 -0300

    upload dos arquivos

---
+++ script.js
@@ -0,0 +1,15 @@
+function sim() {
+    alert("Tamo juntoo 🤙🏻❤️ ");
+}
+
+function desvia(btn) {
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
